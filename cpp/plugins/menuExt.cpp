b0a475df660110f9950e9e706187cdc000e414a2
kyoiryi <kyoiryir@gmail.com>
2026-04-12 11:38:18 +0800

    fix: Hybrid bind MenuItemExt using RAW_CALLBACK for methods with arguments to completely bypass broken T1 conversion on iOS, while retaining standard property bindings for 0-argument getters

--- cpp/plugins/menuExt.cpp
+++ cpp/plugins/menuExt.cpp
@@ -16,22 +16,24 @@ NCB_PRE_REGIST_CALLBACK(menu_dll_stub);
 // ----------------------------------------------------------------------------
 class MenuItemExt {
 public:
-    static tjs_int textToKeycode(const tjs_char* text) {
-        return 0;
+    static tjs_error textToKeycode(tTJSVariant *result, tjs_int numparams, tTJSVariant **param, iTJSDispatch2 *objthis) {
+        if (result) *result = 0;
+        return TJS_S_OK;
     }
 
-    static const tjs_char* keycodeToText(tjs_int keycode) {
-        return TJS_W("");
+    static tjs_error keycodeToText(tTJSVariant *result, tjs_int numparams, tTJSVariant **param, iTJSDispatch2 *objthis) {
+        if (result) *result = TJS_W("");
+        return TJS_S_OK;
     }
 
-    static tjs_int getHMENU() {
+    tjs_int getHMENU() const {
         return 0;
     }
 };
 
 NCB_ATTACH_CLASS(MenuItemExt, MenuItem) {
-    NCB_METHOD(textToKeycode);
-    NCB_METHOD(keycodeToText);
+    NCB_METHOD_RAW_CALLBACK(textToKeycode, MenuItemExt::textToKeycode, 0);
+    NCB_METHOD_RAW_CALLBACK(keycodeToText, MenuItemExt::keycodeToText, 0);
     NCB_PROPERTY_RO(HMENU, getHMENU);
 }
 
